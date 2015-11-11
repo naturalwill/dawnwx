666bbf78986d0b391cc6fe6ca9cd478458539420
Jier <naturalwill@qq.com>
2015-11-12 02:03:57 +0800

    写运行日志

--- common.php
+++ common.php
@@ -12,4 +12,39 @@ if(!@include_once(S_ROOT.'./config.php')) {
 	exit();
 }
 
+//时间
+$mtime = explode(' ', microtime());
+$_SGLOBAL['timestamp'] = $mtime[1];
+
+//写运行日志
+function runlog($log, $file='wechat', $halt=0) {
+	global $_SGLOBAL, $_SERVER;
+
+	$nowurl = $_SERVER['REQUEST_URI']?$_SERVER['REQUEST_URI']:($_SERVER['PHP_SELF']?$_SERVER['PHP_SELF']:$_SERVER['SCRIPT_NAME']);
+	$log = gmdate('Y-m-d H:i:s', $_SGLOBAL['timestamp'])."\t$type\t"."\t{$nowurl}\t".str_replace(array("\r", "\n"), array(' ', ' '), trim($log))."\n";
+	$yearmonth = gmdate('Ym', $_SGLOBAL['timestamp']);
+	$logdir = './data/log/';
+	if(!is_dir($logdir)) mkdir($logdir, 0777);
+	$logfile = $logdir.$yearmonth.'_'.$file.'.php';
+	if(@filesize($logfile) > 2048000) {
+		$dir = opendir($logdir);
+		$length = strlen($file);
+		$maxid = $id = 0;
+		while($entry = readdir($dir)) {
+			if(strexists($entry, $yearmonth.'_'.$file)) {
+				$id = intval(substr($entry, $length + 8, -4));
+				$id > $maxid && $maxid = $id;
+			}
+		}
+		closedir($dir);
+		$logfilebak = $logdir.$yearmonth.'_'.$file.'_'.($maxid + 1).'.php';
+		@rename($logfile, $logfilebak);
+	}
+	if($fp = @fopen($logfile, 'a')) {
+		@flock($fp, 2);
+		fwrite($fp, "<?PHP exit;?>\t".str_replace(array('<?', '?>', "\r", "\n"), '', $log)."\n");
+		fclose($fp);
+	}
+	if($halt) exit();
+}
 ?>
